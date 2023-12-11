e24ceb90aa116695bc14a08ee23182f5272fa27b
Parker <p.cronin.connolly@gmail.com>
2023-12-11 09:52:01 -0500

    second lab 10/10 passing

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// code for excited message
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// for shortGreeting
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
