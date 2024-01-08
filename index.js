ff569a4be64471028b604b579d71403c889ab411
Parker <p.cronin.connolly@gmail.com>
2024-01-08 11:15:11 -0500

    complete

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper';
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const currentUser = "parker"
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`
 
-// code for excited message
+const excitedWelcomeMessage = welcomeMessage.toUpperCase() 
 
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
-// for shortGreeting
-
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
